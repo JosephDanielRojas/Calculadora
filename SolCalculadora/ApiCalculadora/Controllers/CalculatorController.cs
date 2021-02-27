12b292b8154fcf6f80cbb8196921de9efb882db6
Joseph Daniel Rojas Flores <a2019113821@estudiantes.upsa.edu.bo>
2021-02-27 10:01:11 -0400

    Se agrego Get de Suma, Resta, Multiplicacion y Division

--- SolCalculadora/ApiCalculadora/Controllers/CalculatorController.cs
+++ SolCalculadora/ApiCalculadora/Controllers/CalculatorController.cs
@@ -22,11 +22,44 @@
         [Route("Divide")]
         public int Divide([FromHeader] int numA, [FromHeader] int numB)
         {
+            if(numB==0)
+            {
+                return 0;
+            }
             return (numA / numB);
         }
         [HttpPost]
         [Route("Subtract")]
         public int Subtract([FromHeader] int numA, [FromHeader] int numB)
+        {
+            return (numA - numB);
+        }
+
+        [HttpGet]
+        [Route("Add")]
+        public int Add2( int numA, int numB)
+        {
+            return (numA + numB);
+        }
+        [HttpGet]
+        [Route("Multiply")]
+        public int Multiply2( int numA, int numB)
+        {
+            return (numA * numB);
+        }
+        [HttpGet]
+        [Route("Divide")]
+        public int Divide2(int numA, int numB)
+        {
+            if (numB == 0)
+            {
+                return 0;
+            }
+            return (numA / numB);
+        }
+        [HttpGet]
+        [Route("Subtract")]
+        public int Subtract2(int numA, int numB)
         {
             return (numA - numB);
         }
